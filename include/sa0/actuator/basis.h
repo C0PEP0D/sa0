e753e5620b84bbd6db96a18edae70fcbaff8819d
Rémi Monthiller <monthir@git.inpt.fr>
2022-05-16 10:13:04 +0200

    minor correction

--- include/sa0/actuator/basis.h
+++ include/sa0/actuator/basis.h
@@ -2,9 +2,8 @@
 #define SA0_ACTUATOR_BASIS_H
 #pragma once
 
-#include <memory>
-
-#include "sa0/actuator/actuator.h"
+// lib includes
+#include "sa0/active.h"
 
 namespace sl0 {
 
@@ -14,27 +13,81 @@ namespace sa0 {
 
 template<typename TypeStepPassive>
 class StepBasisSwim : public StepActuator<TypeStepPassive> {
-    public:
-        using Type = StepActuator<TypeStepPassive>;
-        using typename Type::TypeStateVectorDynamic;
-        using TypeSpaceVector = typename TypeStepPassive::TypeSpaceVector;
-    public:
-        StepBasisSwim() : StepActuator<TypeStepPassive>() {
-        }
-        TypeVectorStateDynamic operator()(const TypeStepObject& stepObject, const double* pState, const double& t) const {
-            // Get basis
-            auto basis = stepObject.basis(p_state);
-            // Set dState
-            TypeVectorStateDynamic dState(stepObject->stateSize());
-            dState.fill(0.0);
-            // Set get u view
-            auto u = stepObject.x(dState);
-            u = basis.transpose() * velocity * intensity;
-            return dState;
-        }
-    public:
-        using StepActuator<TypeState, TypeStepObject>::intensity;
-        TypeVector velocity; // Local swim velocity
+	public:
+		using Type = StepActuator<TypeStepPassive>;
+		using typename Type::TypeStateVectorDynamic;
+		using TypeSpaceVector = typename TypeStepPassive::TypeSpaceVector;
+	public:
+		StepBasisSwim(const double& p_velocity, const TypeSpaceVector& p_localDirection) : velocity(p_velocity), localDirection(p_localDirection) {
+		}
+
+		TypeStateVectorDynamic operator()(const double* pState, const double& t, const TypeStepPassive& stepPassive) const override {
+			TypeStateVectorDynamic dState(stepPassive.stateSize());
+			dState.fill(0.0);
+			auto sAxis0 = stepPassive.cAxis(pState, 0);
+			auto sAxis1 = stepPassive.cAxis(pState, 1);
+			auto sAxis2 = stepPassive.cAxis2(pState);
+			stepPassive.x(dState.data()) =  velocity * (
+				localDirection(0) * sAxis0 + localDirection(1) * sAxis1 + localDirection(2) * sAxis2
+			);
+			return dState;
+		}
+	public:
+		double velocity;
+		TypeSpaceVector localDirection;
+};
+
+template<typename TypeStepPassive>
+class StepBasisRotate : public StepActuator<TypeStepPassive> {
+	public:
+		using Type = StepActuator<TypeStepPassive>;
+		using typename Type::TypeStateVectorDynamic;
+		using TypeSpaceVector = typename TypeStepPassive::TypeSpaceVector;
+	public:
+		StepBasisRotate(const TypeSpaceVector& p_angularVelocity) : angularVelocity(p_angularVelocity) {
+		}
+
+		TypeStateVectorDynamic operator()(const double* pState, const double& t, const TypeStepPassive& stepPassive) const override {
+			TypeStateVectorDynamic dState(stepPassive.stateSize());
+			dState.fill(0.0);
+			stepPassive.axis(dState.data(), 0) = angularVelocity.cross(stepPassive.cAxis(pState, 0));
+			stepPassive.axis(dState.data(), 1) = angularVelocity.cross(stepPassive.cAxis(pState, 1));
+			return dState;
+		}
+	public:
+		TypeSpaceVector angularVelocity;
+};
+
+template<typename TypeStepPassive>
+class StepBasisOrient : public StepActuator<TypeStepPassive> {
+	public:
+		using Type = StepActuator<TypeStepPassive>;
+		using typename Type::TypeStateVectorDynamic;
+		using TypeSpaceVector = typename TypeStepPassive::TypeSpaceVector;
+	public:
+		StepBasisOrient(const TypeSpaceVector& p_globalDirection, const double& p_time, const TypeSpaceVector& p_localAxis) : globalDirection(p_globalDirection), time(p_time), localAxis(p_localAxis) {
+		}
+
+		TypeStateVectorDynamic operator()(const double* pState, const double& t, const TypeStepPassive& stepPassive) const override {
+			// Pedley model
+			// // init
+			TypeStateVectorDynamic dState(stepPassive.stateSize());
+			dState.fill(0.0);
+			// // get
+			auto sAxis0 = stepPassive.cAxis(pState, 0);
+			auto sAxis1 = stepPassive.cAxis(pState, 1);
+			auto sAxis2 = stepPassive.cAxis2(pState);
+			// // compute
+			TypeSpaceVector angularVelocity = 1.0/(2.0 * time) * (localAxis(0) * sAxis0 + localAxis(1) * sAxis1 + localAxis(2) * sAxis2).cross(globalDirection);
+			stepPassive.axis(dState.data(), 0) = angularVelocity.cross(stepPassive.cAxis(pState, 0));
+			stepPassive.axis(dState.data(), 1) = angularVelocity.cross(stepPassive.cAxis(pState, 1));
+			// // return
+			return dState;
+		}
+	public:
+		TypeSpaceVector globalDirection;
+		double time;
+		TypeSpaceVector localAxis;
 };
 
 }
